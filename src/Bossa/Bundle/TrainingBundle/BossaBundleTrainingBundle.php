1063d1da41c693b312e6924539a98ac88b2d594d
Marcello Duarte <marcello.duarte@gmail.com>
2012-09-14 09:25:11 +0100

    Create bundle and the first feature narrative
    $> app/console generate:bundle -n --namespace Bossa/Bundle/TrainingBundle --dir src
    $> mkdir -p src/Bossa/Bundle/TrainingBundle/Features/admissions
    $> vim src/Bossa/Bundle/TrainingBundle/Features/admissions/student_enrols_on_a_course.feature
    
    We don't need Behat for starting the conversation. We can jump into describing
    the features. Features folder go under the root of the bundle. Inside the
    Features folder we create as many folders as features we will have. The will
    group the .feature files that contain the different user stories and scenarios
    for that feature.

---
+++ src/Bossa/Bundle/TrainingBundle/BossaBundleTrainingBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Bossa\Bundle\TrainingBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BossaBundleTrainingBundle extends Bundle
+{
+}
---
+++ src/Bossa/Bundle/TrainingBundle/Features/admissions/student_enrols_on_a_course.feature
@@ -0,0 +1,4 @@
+Feature: Student enrols on a course
+  As a student
+  In order to guarantee my place for a course
+  I want a way to formalise that
